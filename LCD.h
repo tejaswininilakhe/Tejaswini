d5156e1b0c48bdc69ece81f4d34c48330b3cd212
tejaswininilakhe <159748008+tejaswininilakhe@users.noreply.github.com>
2024-06-24 10:20:00 +0530

    Add files via upload

---
+++ LCD.h
@@ -0,0 +1,14 @@
+
+sbit LCD_RS at PORTC3_bit;
+sbit LCD_EN at PORTC2_bit;
+sbit LCD_D4 at PORTB4_bit;
+sbit LCD_D5 at PORTB3_bit;
+sbit LCD_D6 at PORTB2_bit;
+sbit LCD_D7 at PORTB1_bit;
+
+sbit LCD_RS_Direction at DDC3_bit;
+sbit LCD_EN_Direction at DDC2_bit;
+sbit LCD_D4_Direction at DDB4_bit;
+sbit LCD_D5_Direction at DDB3_bit;
+sbit LCD_D6_Direction at DDB2_bit;
+sbit LCD_D7_Direction at DDB1_bit;
