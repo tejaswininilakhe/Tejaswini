e7a89c24219264690042b069de239bdd26d88a55
tejaswininilakhe <159748008+tejaswininilakhe@users.noreply.github.com>
2024-07-30 10:50:24 +0530

    Update LCD.h

--- LCD.h
+++ LCD.h
@@ -5,7 +5,7 @@ sbit LCD_D4 at PORTB4_bit;
 sbit LCD_D5 at PORTB3_bit;
 sbit LCD_D6 at PORTB2_bit;
 sbit LCD_D7 at PORTB1_bit;
-
+/**************************************************************/
 sbit LCD_RS_Direction at DDC3_bit;
 sbit LCD_EN_Direction at DDC2_bit;
 sbit LCD_D4_Direction at DDB4_bit;
